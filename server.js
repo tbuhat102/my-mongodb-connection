ddca225b13bf9543348279ba7f0170b83c6d8bd6
Andrei Buhatin <tbuhat102@west-mec.org>
2024-10-30 13:54:44 -0700

    added test file

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const { MongoClient, ObjectId } = require("mongodb");
+const fs = require('fs');
 
-require("dotenv").config();
+if (fs.existsSync('.env')) {
+    require('dotenv').config();
+}
+
 const app = express();
 const port = process.env.PORT || 3000;
 const uri = process.env.MONGO_URI;
@@ -27,23 +31,11 @@ async function startServer() {
 
 startServer();
 
-// Create (Insert) endpoint
-app.post("/insert", async (req, res) => {
-    try {
-        const { collection, document } = req.body;
-        const db = client.db("myDatabase");
-        const result = await db.collection(collection).insertOne(document);
-        res.status(201).send(`Document inserted with ID: ${result.insertedId}`);
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
-});
-
 // Read (Find) endpoint
-app.get("/find/:collection", async (req, res) => {
+app.get("/find/:database/:collection", async (req, res) => {
     try {
-        const { collection } = req.params;
-        const db = client.db("myDatabase");
+        const { database, collection } = req.params;
+        const db = client.db(database);
         const documents = await db.collection(collection).find({}).toArray();
         res.status(200).json(documents);
     } catch (err) {
@@ -51,11 +43,24 @@ app.get("/find/:collection", async (req, res) => {
     }
 });
 
-// Delete endpoint
-app.delete("/delete/:collection/:id", async (req, res) => {
+// Create (Insert) endpoint
+app.post("/insert/:database/:collection", async (req, res) => {
     try {
-        const { collection, id } = req.params;
-        const db = client.db("myDatabase");
+        const { document } = req.body;
+        const { database, collection } = req.params;
+        const db = client.db(database);
+        const result = await db.collection(collection).insertOne(document);
+        res.status(201).send(`Document inserted with ID: ${result.insertedId}`);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+// Delete endpoint
+app.delete("/delete/:database/:collection/:id", async (req, res) => {
+    try {
+        const { database, collection, id } = req.params;
+        const db = client.db(database);
         const result = await db
             .collection(collection)
             .deleteOne({ _id: new ObjectId(id) });
